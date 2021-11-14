ff7c8165e2e5b264ee0963ea0c5085c10f2131dc
Anthony Iacampo <a.iacampo99@gmail.com>
2021-11-14 18:45:01 -0500

    Created a tcp server in client

--- project/client.py
+++ project/client.py
@@ -1,4 +1,6 @@
-import socket, os
+import socket, threading, os
+from random import randint
+from datetime import datetime
 
 from message import message as msg_lib
 from constants import ADDR, BUFFER_SIZE, FORMAT
@@ -6,40 +8,91 @@ from constants import ADDR, BUFFER_SIZE, FORMAT
 class Client:
     def __init__(self):
         self.rq_num = -1
+        self.host = socket.gethostbyname(socket.gethostname())
+        self.tcp_port = 10000
+
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP Socket
-        # self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP Socket
+        self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP Socket
+
+        self.get_port_num()
+        self.start() # Need to start on a thread because client needs to listen, talk to server & talk to other clients
 
     def get_rq_num(self):
         self.rq_num = (self.rq_num + 1) % 8
         return self.rq_num
 
-    def send(self, request):
+    def get_port_num(self):
+        self.tcp_port = randint(10000, 65535)
+
+    def print_log(self, msg: str):
+        date_time = datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+        print('[{}] {}'.format(date_time, msg))
+
+    def start(self):
+        self.print_log('Starting Client...')
+        self.tcp_socket.bind((self.host, self.tcp_port)) # Need to check if port number already exists
+        self.print_log('Client is listening on {}:{}'.format(self.host, self.tcp_port))
+
+        try:
+            self.tcp_socket.listen()
+
+            while(True):
+                conn, addr = self.tcp_socket.accept()
+                new_client_thread = threading.Thread(target=self.handle_request, args=(conn, addr))
+                new_client_thread.daemon = True
+                new_client_thread.start()
+
+        except KeyboardInterrupt:
+            self.stop()
+
+    def stop(self):
+        self.print_log('Client is shutting down...')
+        self.udp_socket.close()
+        self.tcp_socket.close()
+
+    def handle_request(self, conn: socket.socket, addr):
+        self.print_log('New connection from {}:{}'.format(addr[0], addr[1]))
+
+        try:
+            data = conn.recv(1024) # Need to determine buffer size
+            data = data.decode(FORMAT)
+            # Read all data
+            # Use data
+
+        except OSError as err:
+            self.print_log('ERROR: {}'.format(err))
+
+        finally:
+            conn.close()
+            self.print_log('Connection {}:{} closed'.format(addr[0], addr[1]))
+
+    def send_to_udp_server(self, request):
         self.udp_socket.sendto(request, ADDR)
 
         try:
             response, addr = self.udp_socket.recvfrom(BUFFER_SIZE)
             self.udp_socket.settimeout(3.0)
-            print('[SERVER RESPONSE]\n{}\n'.format(response.decode(FORMAT)))
+            self.print_log('Server Response\n{}\n'.format(response.decode(FORMAT)))
         
         except socket.timeout:
-            print('[TIMEOUT] Connection timeout...')
+            self.print_log('Connection timeout. Request not received, send again')
 
     def register(self, name):
-        print('[REGISTER] Sending register request...')
+        self.print_log('Sending register request...')
         paylaod = {
             'ACTION': 'REGISTER',
             'RQ#': self.get_rq_num(), # Do circular cycle of numbers 0 - 7
             'NAME': name, # Have user register name (store in .init file) **Name needs to be unique
-            'IP_ADDRESS': socket.gethostbyname(socket.gethostname()),
+            'IP_ADDRESS': self.host,
             'UDP_SOCKET': '', # UDP socket number it can be reached at by the server
-            'TCP_SOCKET': '' # TCP socket number to be used for file transfer with peers
+            'TCP_SOCKET': self.tcp_port # TCP socket number to be used for file transfer with peers
         }
 
         request = msg_lib.create_request('REGISTER', paylaod)
-        self.send(request)
+        self.send_to_udp_server(request)
 
     def de_register(self, name):
-        print('[DE-REGISTER] Sending de-register request...')
+        self.print_log('Sending de-register request...')
         paylaod = {
             'ACTION': 'DE-REGISTER',
             'RQ#': self.get_rq_num(),
@@ -47,10 +100,10 @@ class Client:
         }
 
         request = msg_lib.create_request('DE-REGISTER', paylaod)
-        self.send(request)
+        self.send_to_udp_server(request)
 
     def publish(self, list: list[str]):
-        print('[PUBLISH] Sending publish request...')
+        self.print_log('Sending publish request...')
         paylaod = {
             'ACTION': 'PUBLISH',
             'RQ#': self.get_rq_num(),
@@ -59,10 +112,10 @@ class Client:
         }
 
         request = msg_lib.create_request('PUBLISH', paylaod)
-        self.send(request)
+        self.send_to_udp_server(request)
 
     def remove(self, list: list[str]):
-        print('[REMOVE] Sending remove request...')
+        self.print_log('Sending remove request...')
         paylaod = {
             'ACTION': 'REMOVE',
             'RQ#':  self.get_rq_num(),
@@ -71,20 +124,20 @@ class Client:
         }
 
         request = msg_lib.create_request('REMOVE', paylaod)
-        self.send(request)
+        self.send_to_udp_server(request)
     
     def retrieve_all(self):
-        print('[RETRIEVE-ALL] Sending retrieve all request...')
+        self.print_log('Sending retrieve all request...')
         paylaod = {
             'ACTION': 'RETRIEVE-ALL',
             'RQ#':  self.get_rq_num()
         }
 
         request = msg_lib.create_request('RETRIEVE-ALL', paylaod)
-        self.send(request)
+        self.send_to_udp_server(request)
 
     def retrieve_infot(self, name):
-        print('[RETRIEVE-INFO] Sending retrieve info request...')
+        self.print_log('Sending retrieve info request...')
         paylaod = {
             'ACTION': 'RETRIEVE-INFO',
             'RQ#':  self.get_rq_num(),
@@ -92,10 +145,10 @@ class Client:
         }
 
         request = msg_lib.create_request('RETRIEVE-INFO', paylaod)
-        self.send(request)
+        self.send_to_udp_server(request)
 
     def search_file(self, file_name):
-        print('[SEARCH-FILE] Sending search file request...')
+        self.print_log('Sending search file request...')
         paylaod = {
             'ACTION': 'SEARCH-FILE',
             'RQ#':  self.get_rq_num(),
@@ -103,7 +156,7 @@ class Client:
         }
 
         request = msg_lib.create_request('SEARCH-FILE', paylaod)
-        self.send(request)
+        self.send_to_udp_server(request)
 
     def update_contact(self, ip_address: str, udp_socket: int, tcp_socket: int):
         pass
