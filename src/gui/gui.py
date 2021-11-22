ea53080e2b8bff08c74d82323fa354b9a6062613
JordanHum <jhum268@gmail.com>
2021-11-22 17:00:04 -0500

    replaced .grid with .place added scrollbar to text widget

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter.constants import DISABLED, NORMAL
+from tkinter.constants import DISABLED, NORMAL, RIGHT, X, Y
 
 def Register():
     return
@@ -8,16 +8,19 @@ window = tk.Tk()
 window.geometry("450x400")
 window.resizable(False, False)
 
-label = tk.Label(text="Name").grid(row=0, column=0)
+label = tk.Label(text="Name").place(x=0, y=0)
 entry = tk.Entry(window, width=40)
-entry.grid(row=0, column=1, padx=5, pady=5)
+entry.place(x=50, y=0)
 
-tk.Button(window, text="Register", command=Register).grid(row=1, column=0, sticky=tk.W, padx=5, pady=5)
-tk.Button(window, text="Deregister", command=Register).grid(row=1, column=1, sticky=tk.W, padx=5, pady=5)
+tk.Button(window, text="Register", command=Register).place(x=0, y=25)
+tk.Button(window, text="Deregister", command=Register).place(x=55, y=25)
+tk.Button(window, text="Func1", command=Register).place(x=120, y=25)
 
-log_text = tk.Text(window, height=20, width=40, state=NORMAL)
-log_text.grid(row=2, column=1)
-log_text.insert(tk.END, "testing")
+scroll = tk.Scrollbar(window)
+
+log_text = tk.Text(window, height=20, width=55, state=NORMAL)
+log_text.place(x=0, y=50)
+log_text.insert(tk.END, "testing\nasfasf\nfasf\ng\nfn\nfnfn\nfnn\nfnfn\nfnf\ndndn\ndddd\nddddd\nddddd\ndddddasfasf\nfasf\ng\nfn\nfnfn\nfnn\nfnfn\nfnf\ndndn\ndddd\nddddd\nddddd\nddddd")
 log_text.configure(state=DISABLED)
 
 window.mainloop()
